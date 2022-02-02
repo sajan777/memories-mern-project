a203595134cb107044bf4b2e9fac1845a6b4a560
sajanarora013 <sajan.arora@grazitti.com>
2022-02-02 17:06:58 +0530

    first commit

---
+++ server/models/postMessage.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema({
+  title: { type: String, require: true },
+  message: { type: String, require: true },
+  creator: { type: String, require: true },
+  tags: [String],
+  selectedFile: String,
+  likeCount: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+var PostMessage = mongoose.model("PostMessage", postSchema);
+
+export default PostMessage;
